59935661642cb2695f3ce0afbb4eb976170ef18e
Oliver <oliver@pineywoodstech.com>
2024-04-12 12:53:31 -0500

    fix mario/luigi swap patch

--- tools/uberasmtool/level/MarioGFXAndPaletteChangeOnTrigger.asm
+++ tools/uberasmtool/level/MarioGFXAndPaletteChangeOnTrigger.asm
@@ -35,7 +35,7 @@ init:
 	LDA #$00
 	STA !Trigger
 
-    ;if !ChangetoLuigi = 0
+    ;if !ChangeToLuigi = 0
 	LDA.b #!Palette			; set up pointer
 	STA !RAM_PlayerPalPtr
 	LDA.b #!Palette>>8
@@ -46,40 +46,42 @@ init:
 
 	RTL
 
-; !ChangeToLuigi = 0
+!ChangeToLuigi = 0
 
 main:
-    ;if !ChangeToLuigi = 1
-	;LDX $0DB3 ; actually change player to Luigi
-	;TXA
-	;BEQ ToLuigi
-	;STZ $0DB3
-	;BRA PaletteMain
-;ToLuigi:
-	;INC $0DB3
+	WDM #$01
+    ; if !ChangeToLuigi = 1
+	; LDX $0DB3 ; actually change player to Luigi
+	; TXA
+	; BEQ ToLuigi
+	; STZ $0DB3
+	; endif
+	BRA PaletteMain
+ToLuigi:
+	INC $0DB3
 	;endif
-;PaletteMain:
-    ;if !ChangetoLuigi = 0
+PaletteMain:
+    ;if !ChangeToLuigi = 0
     LDA !Trigger
 	BEQ PaletteFlip     ; Don't change to new palette unless RAM set
 	LDA #$01
 	STA !RAM_PalUpdateFlag
 	STA $15E8|!addr
-	RTL
-	;BRA Gfx
+	;RTL
+	BRA Gfx
 PaletteFlip:
 	LDA #$00			; ; can't STZ long address, back to original if trigger flipped back
 	STA !RAM_PalUpdateFlag
 	STA $15E8|!addr
 	;endif
-; Gfx:
-;     lda !Trigger
-; 	beq BackToMario
-;     rep #$30
-;     lda.w #!CustomExGFXNumber
-;     jsl mario_exgfx_upload_player
-;     sep #$30
-;     RTL
+Gfx:
+    lda !Trigger
+	beq BackToMario
+    rep #$30
+    lda.w #!CustomExGFXNumber
+    jsl mario_exgfx_upload_player
+    sep #$30
+    RTL
 BackToMario:
 	rep #$30
     lda.w #!VanillaExGFXNumber
