30f3ac5f3b4a0d0417de83039f06be52855487a7
Oliver <oliver@pineywoodstech.com>
2026-07-29 11:11:46 -0500

    Update level 10B and enhance UberASM tool dependencies. Integrate new autoscroll functionality in MarioGFXAndPaletteChangeOnTriggerAndPressurePlatesAndMoveLuigiToFront.asm. Modify workspace and binary files for consistency.

--- tools/uberasmtool/level/MarioGFXAndPaletteChangeOnTriggerAndPressurePlatesAndMoveLuigiToFront.asm
+++ tools/uberasmtool/level/MarioGFXAndPaletteChangeOnTriggerAndPressurePlatesAndMoveLuigiToFront.asm
@@ -50,11 +50,15 @@ init:
 	STA !RAM_PlayerPalPtr+2
 	;endif
 
+	JSL lvl10BAutoScroll_init
+
 	RTL
 
 ; !ChangeToLuigi = 0
 
 main:
+	JSL lvl10BAutoScroll_main
+
 	WDM #$01
 	JSL PressurePlates_main
 	; Reorder first so both indices identify their final slots, then perform
